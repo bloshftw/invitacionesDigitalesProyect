0748d377907f914ff3002a775bd1ff8aac6902c6
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2025-04-19 04:17:43 -0300

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+
+fontFamily: {
+  poppins: ['Poppins', 'sans-serif'],
+  dm: ['DM Sans', 'sans-serif'],
+  nunito: ['Nunito', 'sans-serif'],
+  quicksand: ['Quicksand', 'sans-serif'],
+},
+
       colors: {
         primario: '#A5D8FF',
     secundario: '#FBC4CB',
