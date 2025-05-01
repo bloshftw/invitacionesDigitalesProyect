6a5e9e45582e5e8e04322df5cad4780951791243
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2025-04-30 23:24:02 -0300

    commit
    Se rediseño la seccion para elegir el tipo de personalizacion de la tarjeta.

--- js/carrusel.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    effect: 'fade',
-    speed: 1000,
-  });
